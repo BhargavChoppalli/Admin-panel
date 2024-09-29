370f52d6dc5ebac463df90b027b7606e901b4b7a
Bhargav Choppalli <bhargavchoppalli1998@gmail.com>
2024-09-29 22:18:44 +0530

    changed browser to hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Settings from './pages/Settings'
@@ -7,7 +7,7 @@ import Settings from './pages/Settings'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
        <Routes>
           <Route path="/" exact element={<Home/>}/>
           <Route path="about" exact element={<About/>}/>
@@ -15,7 +15,7 @@ const App = () => {
        </Routes>
        
 
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
