49a71b07272f8f52d1727791874c27c7dada2d5f
BhargavChoppalli <112953733+BhargavChoppalli@users.noreply.github.com>
2024-09-29 12:38:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import App from './App'; // Assuming App.jsx is in the same folder
+
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
